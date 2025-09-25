abf5605a150df3c58f68f559f48b3c5571f32db6
lmoboy <steamgoyu19@gmail.com>
2025-09-25 09:38:51 +0300

    feat: implemented simple chat preview with retrieval from the backend

--- backend/chat/chatGetter.go
+++ backend/chat/chatGetter.go
@@ -1,20 +1,19 @@
 package chat
 
 import (
-	"fmt"
 	"net/http"
 	"skillswap/backend/database"
 	"skillswap/backend/utils"
 )
 
-func GetMessagesFromChatId(w http.ResponseWriter, req *http.Request) {
+func GetMessagesFromUID(w http.ResponseWriter, req *http.Request) {
 	chatId := req.URL.Query().Get("cid")
 	res, err := database.Query(`
 	SELECT u.id, u.username, u.email, u.profile_picture, u.aboutme, u.profession, u.location, m.content, m.created_at
 	FROM messages AS m
 	JOIN chats AS c ON m.chat_id = c.id
 	JOIN users AS u ON m.sender_id = u.id
-	WHERE c.id = ?`, chatId)
+	WHERE c.id = ? ORDER BY m.id DESC`, chatId)
 	if err != nil {
 		utils.HandleError(err)
 		utils.SendJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": "Failed to get chat messages"})
@@ -31,8 +30,8 @@ func GetMessagesFromChatId(w http.ResponseWriter, req *http.Request) {
 		}
 		contents = append(contents, content)
 	}
+	utils.DebugPrint("gotta send the messages from: ", chatId)
 
-	fmt.Printf("%v", contents)
 	utils.SendJSONResponse(w, http.StatusOK, map[string]interface{}{"messages": contents})
 }
 
@@ -52,13 +51,16 @@ func GetChatsFromUserID(w http.ResponseWriter, req *http.Request) {
 	var contents []Chat
 	for res.Next() {
 		var content Chat
-		err := res.Scan(&content.Id, &content.Initiator, &content.Responder, &content.Created_at)
+		err := res.Scan(
+			&content.Id, &content.Initiator, &content.Responder, &content.Created_at,
+		)
 		if err != nil {
 			utils.HandleError(err)
 			return
 		}
 		contents = append(contents, content)
 	}
-	utils.DebugPrint(contents)
+	// utils.DebugPrint(contents)
+	utils.DebugPrint("so we got the messages here: ", userId)
 	utils.SendJSONResponse(w, http.StatusOK, contents)
 }
