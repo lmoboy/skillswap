d63597fb17376b452c1c42f10df909b5df9c5d4f
lmoboy <steamgoyu19@gmail.com>
2025-09-26 10:36:49 +0300

    NOOO

--- frontend/src/routes/swapping/[id]/+page.js
+++ frontend/src/routes/swapping/[id]/+page.js
@@ -11,8 +11,7 @@ export const load = async ({ params, fetch }) => {
         const u1id = $auth.user.id;
 
         const res = await fetch(`/api/createChat?u1=${u1id}&u2=${u2id}`);
-
-        return "nigger";
+;
     } catch (error) {
         console.log(error)
     }
